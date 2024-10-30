d01e7b9f4e773f44f4ae320bf395e94d418f6d78
Philippe Deschesnes <philippe.deschesnes@hotmail.com>
2024-10-30 01:03:41 +0000

    remove test function

--- api/src/functions/test.ts
+++
@@ -1,8 +0,0 @@
-import { logger } from 'src/lib/logger'
-
-export const config = {}
-
-export default function handler(_req: Request) {
-  logger.debug('HALO')
-  return new Response('Hello World')
-}
