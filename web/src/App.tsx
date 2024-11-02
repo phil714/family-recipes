6d58e9998d628d620e1c696234f8b98380b3a4cc
Philippe Deschesnes <philippe.deschesnes@hotmail.com>
2024-11-02 13:49:55 +0000

    chore: Add web vitals

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './scaffold.css'
-import './index.css'
 import { AuthProvider, useAuth } from './auth'
+import './index.css'
+import './scaffold.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
+    <Analytics />
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
         <RedwoodApolloProvider useAuth={useAuth}>
