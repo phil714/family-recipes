c91de23415b7d49878734be4239cc5e6db1eab8f
Philippe Deschesnes <philippe.deschesnes@hotmail.com>
2025-01-20 00:44:18 +0000

    envelop

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -2,7 +2,6 @@ import * as Sentry from '@sentry/node'
 
 if (process.env.SENTRY_ENABLED === 'true')
   Sentry.init({
-
     dsn: process.env.SENTRY_DSN,
     environment: process.env.NODE_ENV,
     integrations: [Sentry.prismaIntegration(), Sentry.httpIntegration()],
